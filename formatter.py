211ffdb461d685957a5d7bd17c02fb64c700399d
Alex <shantemgardenix@gmail.com>
2023-05-19 18:45:37 +0500

    added argparser

--- formatter.py
+++ formatter.py
@@ -2,13 +2,20 @@ from docx import Document
 
 from main_req_formatter import MainRequirementsFormatter
 from source_links_formatter import SourceLinksFormatter
+import argparse
 
 
 def main():
-    doc = Document("Диплом.docx")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path_to_docx', type=str, help='Path to docx file')
+    args = parser.parse_args()
+
+    doc = Document(args.path_to_docx)
+
     MainRequirementsFormatter.format_document(doc)
     MainRequirementsFormatter.change_title_page_year(doc, '2023')
     SourceLinksFormatter.check_for_links_presence(doc)
+
     doc.save("edited.docx")
 
 
